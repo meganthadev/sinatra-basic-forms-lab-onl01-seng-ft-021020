0316b3f055e14f3fb0b5612233fe830c68bba58d
Megan Horsfield <meganthadev@gmail.com>
2020-04-08 23:07:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  get '/index' do 
+   erb :index   
+  end  
+  
+  
   post '/display_puppy' do 
+    text = params[:user_input]
     
+    erb :display_puppy
   end   
 
 end
